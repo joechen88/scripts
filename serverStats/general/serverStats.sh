eaa80fcad8ce374820c35f4b5090f61329b9e2bc
Joe Chen <joechen@joechen-m01.vmware.com>
2018-08-06 13:52:33 -0700

    updated serverStats.sh for jenkins and prme-general scripts

--- serverStats/general/serverStats.sh
+++ serverStats/general/serverStats.sh
@@ -14,14 +14,20 @@ echo "<pre>" >> ${thePath}server-stats.html
 echo "<b>current process: </b>" >> ${thePath}server-stats.html
 ps au | grep -iEv "root" | grep -iEv "ps au" >> ${thePath}server-stats.html
 echo "<br><br>" >> ${thePath}server-stats.html
+echo "<b>Zombie process:</b>" >> ${thePath}server-stats.html
+ps aux | grep -w defunct | grep -iEv "grep --color=auto|grep -w defunct" >> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
 echo "<b>Memory usage:</b>" >> ${thePath}server-stats.html
 echo "<br>" >> ${thePath}server-stats.html
 free -h >> ${thePath}server-stats.html
 echo "<br><br>" >> ${thePath}server-stats.html
 echo "<b>TOP status:</b>" >> ${thePath}server-stats.html
-top -c -b -n 1 | head -6 >> ${thePath}server-stats.html
+top -c -b -n 1 | head -7 >> ${thePath}server-stats.html
 echo "" >> ${thePath}server-stats.html
 ps au | grep -iEv "grep --color=auto cron">> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
+echo "<b>Storage:</b>" >> ${thePath}server-stats.html
+df -h | grep -iE "sda1|vsan-cert|Filesystem" >> ${thePath}server-stats.html
 echo "</pre>" >> ${thePath}server-stats.html
 echo "</body>" >> ${thePath}server-stats.html
 echo "</html>" >> ${thePath}server-stats.html
--- serverStats/jenkins/serverStats.sh
+++ serverStats/jenkins/serverStats.sh
@@ -14,12 +14,24 @@ echo "<pre>" >> ${thePath}server-stats.html
 echo "<b>jenkin's process: </b>" >> ${thePath}server-stats.html
 ps au | grep -iE "jenkins" | grep -iEv "grep" >> ${thePath}server-stats.html
 echo "<br><br>" >> ${thePath}server-stats.html
+echo "<b>misc process: </b>" >> ${thePath}server-stats.html
+ps aux | grep -iE "vsanhol-nfs-array|nimbus" | grep -iEv "grep --color|grep -iE vsanhol-nfs-array|nimbus" >> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
+echo "<b>Zombie process:</b>" >> ${thePath}server-stats.html
+ps aux | grep -w defunct | grep -iEv "grep --color=auto|grep -w defunct" >> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
 echo "<b>Memory usage:</b>" >> ${thePath}server-stats.html
 echo "<br>" >> ${thePath}server-stats.html
 free -h >> ${thePath}server-stats.html
 echo "<br><br>" >> ${thePath}server-stats.html
 echo "<b>TOP status:</b>" >> ${thePath}server-stats.html
-top -c -b -n 1 -u jenkins >> ${thePath}server-stats.html
+top -b -n 1 | head -7 >> ${thePath}server-stats.html
+echo ""  >> ${thePath}server-stats.html
+ps aux | grep -iE "jenkins" | grep -iEv "grep -iE jenkins" >> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
+echo "<b>Storage:</b>" >> ${thePath}server-stats.html
+df -h | grep -iE "dm-0|vsan-cert|Filesystem" >> ${thePath}server-stats.html
+echo "<br><br>" >> ${thePath}server-stats.html
 echo "</pre>" >> ${thePath}server-stats.html
 echo "</body>" >> ${thePath}server-stats.html
 echo "</html>" >> ${thePath}server-stats.html
